10203d7cc7657626726bdeccf69b7a22532c7701
Dikshant Tanwar <dikshant@cyntexa.com>
2026-02-18 17:06:09 +0530

    Basic React Setup Done!

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./Body";
+import Home from "./Home";
+import Login from "./Login";
+
 function App() {
   return (
     <>
-      <div className="navbar bg-base-300 shadow-sm">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl">PullRequest 💘</a>
-        </div>
-        <div className="flex gap-2">
-          <div className="dropdown dropdown-end px-5">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle avatar"
-            >
-              <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                />
-              </div>
-            </div>
-            <ul
-              tabIndex="-1"
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
-              <li>
-                <a>Settings</a>
-              </li>
-              <li>
-                <a>Logout</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      
-      <h1 className="font-bold text-4xl m-auto text-blue-800">Vite + React</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route index element={<Home />} />
+            <Route path="/signup" element={"Welcome to the Page - Signup Please"} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/feed" element={"Feed Page!!"}/>
+            <Route path="/connections" element={"Connections Page!!"}/>
+
+
+            <Route path="*" element={"404 Not Found!"} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/Login.jsx
@@ -0,0 +1,17 @@
+const Login = () => {
+  return (
+    <>
+      <div>
+        <h1 className="text-4xl text-blue-400 text-center font-medium ">
+          Welcome to Login Page
+        </h1>
+        <form action="#">
+          <input placeholder="First Name" type="text" />
+          <input placeholder="Last Name" type="text" />
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Login;
